cd13f7fba97ff305fec86ec7cf2108559158fc6c
Lauren <laurenmbotha@gmail.com>
2020-07-13 16:54:49 +0000

    Image is able to upload, TO DO: Coordinate with JSOn and return image with comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -25,16 +25,17 @@ function getData() {
   });
 }
 
-// function fetchBlobstoreUrlAndShowForm() {
-//   fetch('/blobstore-upload-url')
-//       .then((response) => {
-//         return response.text();
-//       })
-//       .then((imageUploadUrl) => {
-//         const messageForm = document.getElementById('my-form');
-//         messageForm.action = imageUploadUrl;
-//         messageForm.classList.remove('hidden');
-//       });
-// }
+function fetchBlobstoreUrlAndShowForm() {
+  fetch('/blobstore-upload-url')
+      .then((response) => {
+        console.log(response);
+        return response.text();
+      })
+      .then((imageUploadUrl) => {
+        const messageForm = document.getElementById('my-form');
+        messageForm.action = imageUploadUrl;
+        messageForm.classList.remove('hidden');
+      });
+}
 
     
